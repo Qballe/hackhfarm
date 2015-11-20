f3c5e2267746740829349462ecb76a219595ee72
Alex Zanardo <info@alexzanardo.com>
2015-11-20 17:19:57 +0100

    Attach search & fix

--- application/models/Search_model.php
+++ application/models/Search_model.php
@@ -13,22 +13,37 @@ class Search_model extends CI_Model {
     //$this->db->join('people', 'people.id = roles.person_id', 'left');
     $this->db->like('name', $terms);
     $this->db->or_like('description', $terms);
+    $this->db->or_like('email', $terms);
+    $this->db->or_like('contacts', $terms);
 
     $query = $this->db->get();
-    return $query->result_array();
+    return $query->result();
 	}
 
 	public function by_user($terms){
-    $this->db->select('people.*');
+    $this->db->select('users.*');
     //$this->db->select('companies.name AS company_name, companies.id AS company_id');
-    $this->db->from('people');
-    //$this->db->join('roles', 'roles.person_id = people.id', 'left');
+    $this->db->from('users');
+    //$this->db->join('roles', 'roles.person_id = users.id', 'left');
     //$this->db->join('companies', 'companies.id = roles.company_id', 'left');
-    $this->db->like('name', $terms);
-    $this->db->or_like('description', $terms);
+    $this->db->like('first_name', $terms);
+    $this->db->or_like('last_name', $terms);
+    $this->db->or_like('email', $terms);
 
     $query = $this->db->get();
-    return $query->result_array();
+
+	  $result1 = $query->result();
+	  $result = array();
+
+	  foreach ($result1 as $row) {
+	      $id = $row->id;
+	      $query2=$this->db->get_where('users_details', array('user_id' => $id));
+	      $result2=$query2->row_array();
+
+	      array_push($result, array_merge((array)$row, $result2) );
+	  }
+
+	  return (object)$result;
 	}
 
 	public function by_skill($terms){
@@ -38,7 +53,7 @@ class Search_model extends CI_Model {
     $this->db->or_like('description', $terms);
 
     $query = $this->db->get();
-    return $query->result_array();
+    return $query->result();
 	}
 
 	public function all($terms){
