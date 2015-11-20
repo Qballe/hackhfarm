c21978aaa466f06db5fffad3827ec78015f40325
ChiaraBi <chiara.bigarella1@gmail.com>
2015-11-20 16:39:34 +0100

    added link to companies view

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,4 +53,4 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['companies/(:any)'] = 'companies/details';
-$route['users/(:any)'] = 'users/details';
+$route['users/(:num)'] = 'users/details';
--- application/controllers/Companies.php
+++ application/controllers/Companies.php
@@ -14,34 +14,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @author fabioros
  */
 class Companies extends CI_Controller{
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Users_model');
+    }
 
     public function index() {
-                
-                $companies= array();
-                
+        $companies=$this->Company_model->read();
+                /*$companies= array();
+
                 for ($i =0;$i<30; $i++ ){
                 $company['name']="Company_{$i}";
                 $company['email']="a{$i}@a{$i}.a";
                 $company['description']="Company_{$i}_description Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tellus nibh, aliquam lobortis ligula sed, eleifend sodales est. Quisque accumsan nisi quis massa fermentum, quis finibus magna porttitor. Aliquam egestas justo vel lectus scelerisque volutpat. Nunc aliquet neque at elit faucibus, in pellentesque metus scelerisque. Sed tincidunt sagittis blandit. ";
                 $company['contacts']="Company_{$i}_contacts";
                 $companies[]=$company;
-                }
+                }*/
 		//$this->load->view('home_page');
                 //$data['title'] = ucfirst($page); // Capitalize the first letter
-                $data['first_button_text'] = "List view";
-                $data['second_button_text'] = "Map view";
-                $data['companies'] = $companies;
-
+        $data['first_button_text'] = "List view";
+        $data['second_button_text'] = "Map view";
+        $data['companies'] = $companies;
+        $data['title']="Companies";
                 
-                $data['title']="Companies";
-                
-                $this->load->view('templates/topbar',$data);
-                $this->load->view('templates/header');
-                $this->load->view('companies', $companies);
+        $this->load->view('templates/topbar',$data);
+        $this->load->view('templates/header');
+        $this->load->view('companies', $companies);
                 #//$this->load->view('home_page');
-                $this->load->view('templates/footer',$data);
-                
-                
+        $this->load->view('templates/footer',$data);
 	}
     public function details($id) {
         $company=$this->Company_model->getById($id);
