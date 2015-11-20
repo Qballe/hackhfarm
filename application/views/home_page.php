de8642bfbd2c9f8c2bc4d3a9f1295669098ca28f
Fabio Ros <fabio.ros90@gmail.com>
2015-11-20 16:58:14 +0100

    home page done

--- application/views/home_page.php
+++ application/views/home_page.php
@@ -1,6 +1,9 @@
 <body>
     <div id="wrap">
         <div id="main" class="container clear-top">
+            <br /> &nbsp;<br /> &nbsp;<br /> &nbsp;
+            <center><img src="resources/img/icons/logo.png"></img></center>
             <h1 class="home_title"> WAY </h1>
+            <h2 class="home_subtitle"> Who are you? </h1>
         </div>
     </div>
