19ecbdece2c20d6f7350f06834603d2f64cd936f
Fabio Ros <fabio.ros90@gmail.com>
2015-11-20 15:23:40 +0100

    ++

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,9 +1,11 @@
 <html>
         <head>
                 <title> WAY</title>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
                 <?php
                  require_once 'resources/bootstrap.php';
                 ?><link rel="stylesheet" type="text/css" href="resources/css/style.css">
+                
                 <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,900italic,900,700,700italic,500italic,500,400italic,300italic,300,100italic&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
         </head>
         <body >
